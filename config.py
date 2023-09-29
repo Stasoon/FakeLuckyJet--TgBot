e929c971cbb2ddd3d97d457dde4f648918d4939f
Stasoon <stas.csa@ya.ru>
2023-09-29 18:56:59 +0300

    config updated

--- config.py
+++ config.py
@@ -11,8 +11,6 @@ CODE_WORD = 'BOTJET11'
 
 class Config:
     TOKEN: Final = os.getenv('BOT_TOKEN', 'Впишите токен в .env!')
-    ADMIN_IDS: Final = tuple(int(i) for i in str(os.getenv('BOT_ADMIN_IDS')).split(','))
-
     __BOT_USERNAME = 'bot_username'
 
     @classmethod
@@ -22,8 +20,5 @@ class Config:
     @classmethod
     def get_bot_username(cls) -> str:
         return cls.__BOT_USERNAME
-
-    LOCALES_DIR = 'locales'
-    I18N_DOMAIN = 'bot'
 
     DEBUG: Final = bool(os.getenv('DEBUG'))
